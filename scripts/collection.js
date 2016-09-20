85e5cd5bdd53a1237ecaf6a32fcd762ca744a694
Rachel <raedesignz1@gmail.com>
2016-09-20 12:35:53 -0400

    Checkpoint 17 JQuery Collection Complete

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate = 
+var buildCollectionItemTemplate = function() {
+    var template = 
     '<div class="collection-album-container column fourth">'
 +       '<img src="assets/images/album_covers/01.png"/>'
 +       '<div class="collection-album-info caption">'
@@ -12,14 +13,19 @@ var collectionItemTemplate =
 +           '</p>'
 +       '</div>'
 +    '</div>'
+    ;
+    return $(template);
+};
 
-window.onload = function() {
+$(window).load(function() {
     
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    var $collectionContainer = $('.album-covers');
     
-    collectionContainer.innerHTML = '';
+    $collectionContainer.empty();
     
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
